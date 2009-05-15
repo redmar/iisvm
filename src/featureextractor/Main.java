f4df6afcb50dcc45f1e7da4038cfa003490c0e73
benoist <benoist.claassen@gmail.com>
2009-05-15 17:21:27 +0200

    Feature extractor finished

--- src/featureextractor/Main.java
+++ src/featureextractor/Main.java
@@ -16,60 +16,62 @@ import org.apache.lucene.analysis.*;
  * @author benoist
  */
 public class Main {
-    private static String[] testFiles = new String[]{"p7.jpg", "p24.jpg","p27.jpg","p51.jpg", "p57.jpg","p67.jpg"};
-    private static String testFilesPath = "/Users/benoist/Pictures/";
+    private static String testFilesPath = "dataset";
     private static String indexPath = "test-index";
 
     public static void testCreateIndex() throws IOException {
         // Create an appropriate DocumentBuilder
-        DocumentBuilder builder = DocumentBuilderFactory.getExtensiveDocumentBuilder();
+        DocumentBuilder builder = DocumentBuilderFactory.getFastDocumentBuilder();
         IndexWriter iw = new IndexWriter(indexPath, new SimpleAnalyzer(), true);
-        for (String identifier : testFiles) {
-            // Build the Lucene Documents
-            Document doc = builder.createDocument(new FileInputStream(testFilesPath +
-				identifier), identifier);
-            // Add the Documents to the index
-            iw.addDocument(doc);
+        File root = new File(testFilesPath);
+        for (File dir : root.listFiles()) {
+            if (dir.isDirectory()){
+                int index = 0;
+                for (File file : dir.listFiles()){
+                    if (!file.isDirectory() && file.getName().endsWith("jpg")){
+                        if (index > 29){
+                            break;
+                        }
+                        index++;
+                        // Build the Lucene Documents
+                        Document doc = builder.createDocument(new FileInputStream(file), dir.getName() +"/"+file.getName());
+                        iw.addDocument(doc);
+                    }
+                }
+            }
         }
         iw.optimize();
         iw.close();
     }
 
-    public static void testSearch() throws IOException {
+    public static void testSearch(String filename) throws IOException {
 		// Opening an IndexReader
         IndexReader reader = IndexReader.open(indexPath);
 		// Creating an ImageSearcher
         ImageSearcher searcher = ImageSearcherFactory.createDefaultSearcher();
 		// Reading the sample image, which is our "query"
-        FileInputStream imageStream = new FileInputStream(testFilesPath + testFiles[0]);
+        FileInputStream imageStream = new FileInputStream(filename);
         BufferedImage bimg = ImageIO.read(imageStream);
 		// Search for similar images
         ImageSearchHits hits = null;
 		hits = searcher.search(bimg, reader);
 		// print out results
-        for (int i = 0; i < 6; i++) {
-            System.out.println(hits.score(i) + ": " +
-				hits.doc(i).getField(DocumentBuilder.FIELD_NAME_IDENTIFIER).stringValue());
-        }
-
-		// Get a document from the results
-        Document document = hits.doc(0);
-		// Search for similar Documents based on the image features
-		hits = searcher.search(document, reader);
-        for (int i = 0; i < 6; i++) {
-            System.out.println(hits.score(i) + ": " +
-				hits.doc(i).getField(DocumentBuilder.FIELD_NAME_IDENTIFIER).stringValue());
+        for (int i = 0; i < 20; i++) {
+            System.out.println(hits.doc(i).getField(DocumentBuilder.FIELD_NAME_IDENTIFIER).stringValue() + " " + hits.score(i));
         }
     }
-
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
-            testCreateIndex();
-            testSearch();
+            if (args.length > 0){
+                testSearch(args[0]);
+            } else {
+                testCreateIndex();
+            }
+
         } catch (IOException e)
         {
             e.printStackTrace();
